1197da338fd209c2eed1b617c5e4f970cc464d2c
wedaft <trevor.a.gill@gmail.com>
2017-10-12 14:26:18 -0700

    add buttons to main activity

--- app/src/main/java/com/epicodus/recipro/MainActivity.java
+++ app/src/main/java/com/epicodus/recipro/MainActivity.java
@@ -2,12 +2,35 @@ package com.epicodus.recipro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.createRecipe) Button mCreateRecipe;
+    @Bind(R.id.findRecipe) Button mFindRecipe;
+    @Bind(R.id.groceryList) Button mGroceryList;
+    @Bind(R.id.recipeBox) Button mRecipeBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mCreateRecipe) {
+            
+        } else if(v == mRecipeBox) {
+
+        } else if(v == mFindRecipe) {
+
+        } else if(v == mGroceryList) {
+
+        }
     }
 }
