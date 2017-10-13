1e6d4277eae5147e43fe95500f675ffbf4f13ad0
Trevor Gill <trevor.a.gill@gmail.com>
2017-10-13 09:42:24 -0700

    add code working toward dynamically populating ingredient list view

--- app/src/main/java/com/epicodus/recipro/NewRecipeActivity.java
+++ app/src/main/java/com/epicodus/recipro/NewRecipeActivity.java
@@ -4,9 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -16,6 +21,9 @@ public class NewRecipeActivity extends AppCompatActivity implements View.OnClick
     @Bind(R.id.ingredientCount) EditText mIngredientCount;
     @Bind(R.id.ingredientMeasurement) Spinner mIngredientMeasurement;
     @Bind(R.id.ingredientName) EditText mIngredientName;
+    @Bind(R.id.listView) ListView mListView;
+    ArrayList<String> ingredientList = new ArrayList<>();
+    ArrayAdapter<ArrayList<String>> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +31,28 @@ public class NewRecipeActivity extends AppCompatActivity implements View.OnClick
         setContentView(R.layout.activity_new_recipe);
         ButterKnife.bind(this);
         mAddIngredientButton.setOnClickListener(this);
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, ingredientList);
+        mListView.setAdapter(adapter);
+
     }
 
     public void onClick(View v) {
         if(v == mAddIngredientButton) {
-            String name = mIngredientName.getText().toString();
-            String measurement = mIngredientMeasurement.getSelectedItem().toString();
-            String count = mIngredientCount.getText().toString();
-            clearIngredientInputs();
-            Log.i("NewRecipeActivity", "Data" + count + name + measurement);
+            String ingredient = createIngredientString();
+            ingredientList.add(ingredient);
+//            clearIngredientInputs();
+            adapter.add(ingredientList);
+            adapter.notifyDataSetChanged();
+            Log.i("NewRecipeActivity","List includes: " + ingredientList);
         }
+    }
+
+    public String createIngredientString() {
+        String name = mIngredientName.getText().toString();
+        String measurement = mIngredientMeasurement.getSelectedItem().toString();
+        String count = mIngredientCount.getText().toString();
+        String ingredient = String.format("%s %s %s", count, measurement, name);
+        return ingredient;
     }
 
     public void clearIngredientInputs() {
@@ -40,4 +60,5 @@ public class NewRecipeActivity extends AppCompatActivity implements View.OnClick
         mIngredientCount.setText("");
         mIngredientMeasurement.setSelection(0);
     }
+
 }
