d77c535bc7cc7d2059fb9c79c84ee097e0810d58
Trevor Gill <trevor.a.gill@gmail.com>
2017-10-15 17:11:13 -0700

    add layout for find recipes activity

---
+++ app/src/main/java/com/epicodus/recipro/FindRecipesActivity.java
@@ -0,0 +1,15 @@
+package com.epicodus.recipro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.epicodus.myrestaurants.R;
+
+public class FindRecipesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_recipes);
+    }
+}
