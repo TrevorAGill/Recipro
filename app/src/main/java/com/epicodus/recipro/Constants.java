40de35566d566ff4b9dd8777f84b9885432f8fcb
wedaft <trevor.a.gill@gmail.com>
2017-10-17 14:30:21 -0700

    add Yummly API credentials to gradle

---
+++ app/src/main/java/com/epicodus/recipro/Constants.java
@@ -0,0 +1,6 @@
+package com.epicodus.recipro;
+
+public class Constants {
+    public static final String YUMMLY_APPLICATION_ID = BuildConfig.YUMMLY_APPLICATION_ID;
+    public static final String YUMMLY_KEY = BuildConfig.YUMMLY_KEY;
+}
---
+++ app/src/main/java/com/epicodus/recipro/YummlyService.java
@@ -0,0 +1,5 @@
+package com.epicodus.recipro;
+
+public class YummlyService {
+
+}
