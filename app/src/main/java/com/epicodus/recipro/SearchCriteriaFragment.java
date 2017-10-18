18adc4536cd9e5fb8230b15a63500d3a7fad1214
Guest User <Guest@Epicodus-3B.local>
2017-10-18 14:30:36 -0700

    attempt to add fragments to find recipe activity

--- app/src/main/java/com/epicodus/recipro/SearchCriteriaFragment.java
+++ app/src/main/java/com/epicodus/recipro/SearchCriteriaFragment.java
@@ -1,19 +1,26 @@
 package com.epicodus.recipro;
 
+
 import android.content.Intent;
-import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import com.epicodus.myrestaurants.R;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class FindRecipesActivity extends AppCompatActivity implements View.OnClickListener {
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SearchCriteriaFragment extends Fragment implements View.OnClickListener {
     @Bind(R.id.timeEditText) EditText mTime;
     @Bind(R.id.includeEditText) EditText mAllowedIngredients;
     @Bind(R.id.excludeEditText) EditText mExcludedIngredients;
@@ -21,18 +28,32 @@ public class FindRecipesActivity extends AppCompatActivity implements View.OnCli
     @Bind(R.id.cuisineSpinner) Spinner mCuisine;
     @Bind(R.id.submitCriteriaButton) Button mSubmitCriteriaButton;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_find_recipes);
-        ButterKnife.bind(this);
 
+    public SearchCriteriaFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         mSubmitCriteriaButton.setOnClickListener(this);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_search_criteria, container, false);
+        ButterKnife.bind(this,view);
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search_criteria, container, false);
+
     }
 
     @Override
     public void onClick(View v) {
         if (v == mSubmitCriteriaButton) {
+            FindRecipesActivity.showSearchResults();
             String time = mTime.getText().toString();
             String[] allowedIngredients = mAllowedIngredients.getText().toString().split(",");
             String[] excludedIngredients = mExcludedIngredients.getText().toString().split(",");
@@ -61,9 +82,4 @@ public class FindRecipesActivity extends AppCompatActivity implements View.OnCli
         }
     }
 
-    public void showSearchResults() {
-        SearchResultsFragment searchResultsFragment = new SearchResultsFragment();
-
-
-    }
 }
