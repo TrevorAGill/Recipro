267a234c9269f0ef31f375f8640d949aa73d2244
Trevor Gill <trevor.a.gill@gmail.com>
2017-10-28 17:17:29 -0700

    add ability to logout of firebase account

--- app/src/main/java/com/epicodus/recipro/MainActivity.java
+++ app/src/main/java/com/epicodus/recipro/MainActivity.java
@@ -4,8 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -28,6 +33,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public void onClick(View v) {
         if(v == mCreateRecipe) {
             Intent iCreateRecipe = new Intent(MainActivity.this, NewRecipeActivity.class);
@@ -41,5 +63,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         } else if(v == mGroceryList) {
             Log.i("MainActivity","grocery list");
         }
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 }
