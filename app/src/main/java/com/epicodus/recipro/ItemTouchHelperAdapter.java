5881d3ba74d518f3703677802fd3f90a792725a6
Trevor Gill <trevor.a.gill@gmail.com>
2017-11-05 12:40:53 -0800

    add ability to drag list items in saved recipes

---
+++ app/src/main/java/com/epicodus/recipro/ItemTouchHelperAdapter.java
@@ -0,0 +1,6 @@
+package com.epicodus.recipro;
+
+public interface ItemTouchHelperAdapter {
+    boolean onItemMove(int fromPosition, int toPosition);
+    void onItemDismiss(int position);
+}
--- app/src/main/java/com/epicodus/recipro/SavedRecipeListActivity.java
+++ app/src/main/java/com/epicodus/recipro/SavedRecipeListActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
@@ -14,9 +15,10 @@ import com.google.firebase.database.FirebaseDatabase;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class SavedRecipeListActivity extends AppCompatActivity {
+public class SavedRecipeListActivity extends AppCompatActivity implements OnStartDragListener {
     private DatabaseReference mRecipeReference;
-    private FirebaseRecyclerAdapter mFirebaseAdapter;
+    private FirebaseRecipeListAdapter mFirebaseAdapter;
+    private ItemTouchHelper mItemTouchHelper;
 
     @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
 
@@ -24,9 +26,13 @@ public class SavedRecipeListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_recipe_list);
+        setContentView(R.layout.activity_saved_recipe_list);
         ButterKnife.bind(this);
 
+        setUpFirebaseAdapter();
+    }
+
+    private void setUpFirebaseAdapter() {
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         String uid = user.getUid();
 
@@ -35,27 +41,27 @@ public class SavedRecipeListActivity extends AppCompatActivity {
                 .getReference(Constants.FIREBASE_CHILD_RECIPES)
                 .child(uid);
 
-        setUpFirebaseAdapter();
-    }
+        mFirebaseAdapter = new FirebaseRecipeListAdapter(Recipe.class, R.layout.recipe_list_item_drag, FirebaseRecipeViewHolder.class, mRecipeReference, this, this);
 
-    private void setUpFirebaseAdapter() {
-
-        mFirebaseAdapter = new FirebaseRecyclerAdapter<Recipe, FirebaseRecipeViewHolder>
-                (Recipe.class, R.layout.recipe_list_item, FirebaseRecipeViewHolder.class, mRecipeReference) {
-
-            @Override
-            protected void populateViewHolder(FirebaseRecipeViewHolder viewHolder, Recipe model, int position) {
-                viewHolder.bindRecipe(model);
-            }
-        };
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mRecyclerView.setAdapter(mFirebaseAdapter);
-    }
+
+        ItemTouchHelper.Callback callback = new SimpleItemTouchHelperCallback(mFirebaseAdapter);
+        mItemTouchHelper = new ItemTouchHelper(callback);
+        mItemTouchHelper.attachToRecyclerView(mRecyclerView);
+    };
+
+
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         mFirebaseAdapter.cleanup();
+    }
+
+    @Override
+    public void onStartDrag(RecyclerView.ViewHolder viewHolder) {
+        mItemTouchHelper.startDrag(viewHolder);
     }
 }
